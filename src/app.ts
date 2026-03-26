a078a3bef0b9ebc19a23ad0f4f03c3f1f9ee0576
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-03-26 13:25:22 +0900

    refactor: erase useless comment app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,11 +14,6 @@ db.sequelize
   });
 
 
-// TODO : controller & service & repository
-// TODO : declare router here
-// TODO : dev mysql 연결
-
-// app.js -> models/index.js - db server connection - models/article.js(모델파일) 동기화 -> article 물리 테이블 생성
 app.use((req, res, next) => {
   console.log('요청 들어옴');
   next();
@@ -37,8 +32,3 @@ app.use(express.json());
 app.listen(3000, () => {
   console.log("server running")
 })
-
-//  
-// 로직 설명 : router > controller > service > repository > db 
-// router=경로 매핑 담당
-// service=데이터가공, 검증, 
